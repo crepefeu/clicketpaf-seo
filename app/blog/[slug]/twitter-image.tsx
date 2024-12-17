8a11ea8595dc9638dfe5d7941c37e66d1c49bb67
crepefeu <esteban.rodriguez@live.fr>
2024-12-17 16:20:16 +0100

    Updated twitter card image generation

--- app/blog/[slug]/twitter-image.tsx
+++ app/blog/[slug]/twitter-image.tsx
@@ -2,7 +2,7 @@ import { getArticleBySlug } from "@/app/lib/articleUtils";
 import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
-export const alt = "Blog article thumbnail";
+export const alt = "Couverture de l'article de blog";
 export const size = {
   width: 1200,
   height: 630,
@@ -25,7 +25,20 @@ export default async function Image({ params }: { params: { slug: string } }) {
             justifyContent: "center",
           }}
         >
-          <p style={{ color: "#15803d", fontSize: "48px" }}>Article not found</p>
+          <h1
+            style={{
+              fontSize: "60px",
+              color: "#15803d",
+              textAlign: "center",
+              marginBottom: "20px",
+              fontWeight: "bold",
+            }}
+          >
+            Clicketpaf - Le paiement en ligne simplifié
+          </h1>
+          <p style={{ color: "#15803d", fontSize: "48px" }}>
+            Article introuvable
+          </p>
         </div>
       )
     );
@@ -39,30 +52,40 @@ export default async function Image({ params }: { params: { slug: string } }) {
           width: "100%",
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
           backgroundColor: "#f3f4f6",
           padding: "40px",
         }}
       >
-        <div
+        <h1
+          style={{
+            fontSize: "60px",
+            color: "#15803d",
+            marginBottom: "20px",
+            fontWeight: "bold",
+          }}
+        >
+          Clicketpaf - Le paiement en ligne simplifié
+        </h1>
+        <section
           style={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            gap: "20px",
+            gap: "10px",
           }}
         >
           <h1
             style={{
-              fontSize: "60px",
+              fontSize: "54px",
               color: "#15803d",
               textAlign: "center",
               marginBottom: "20px",
+              fontWeight: "bold",
             }}
           >
             {article.title}
           </h1>
+
           <div
             style={{
               display: "flex",
@@ -72,16 +95,33 @@ export default async function Image({ params }: { params: { slug: string } }) {
             }}
           >
             <p>Par {article.author}</p>
-            <p>•</p>
+            <p
+              style={{
+                color: "#6b7280",
+                fontSize: "24px",
+              }}
+            >
+              •
+            </p>
             <p>{new Date(article.date).toLocaleDateString()}</p>
-            <p>•</p>
+            <p
+              style={{
+                color: "#6b7280",
+                fontSize: "24px",
+              }}
+            >
+              •
+            </p>
             <p>{article.metadata.readingTime}</p>
           </div>
-        </div>
+        </section>
       </div>
     ),
     {
       ...size,
+      headers: {
+        "Cache-Control": "no-store, max-age=0",
+      },
     }
   );
 }
